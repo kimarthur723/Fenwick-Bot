4170d164792c2862cab1caa642ee89a6877ddcf8
kimarthur723 <kimarthur723@gmail.com>
2022-10-04 15:19:32 -0400

    I love object oriented programming !

--- scrapist.py
+++ scrapist.py
@@ -3,41 +3,41 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-# set up chrome driver
-s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
 
-# get room rental website
-driver.get('https://gmu.libcal.com/spaces?lid=1205&gid=2117')
+class Scrapist:
+    def __init__(self):
+        # set up chrome driver
+        s = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=s)
 
-# create dict to store information on each room
-# room will be {roomNumber:[time1,time2,time3...]...}
-rooms = {}
+        # get room rental website
+        self.driver.get('https://gmu.libcal.com/spaces?lid=1205&gid=2117')
 
-# get a list of cell elements in the table
-cells = driver.find_elements(By.CSS_SELECTOR, '.fc-timeline-events .fc-timeline-event-harness .fc-timeline-event')
+    def find_rooms(self):
+        # create dict to store information on each room
+        # room will be {roomNumber:[time1,time2,time3...]...}
+        rooms = {}
 
-for cell in cells:
-    # split the cell title by whitespace
-    # cell titles are in the format "hh:mmxm day, Month dd, yyyy - XXXX - XXXX"
-    celtit = cell.get_attribute('title').split()
-    roomNumber = int(celtit[6])
-    day = int(celtit[3].strip(','))
+        # get a list of cell elements in the table
+        cells = self.driver.find_elements(By.CSS_SELECTOR,
+                                          '.fc-timeline-events .fc-timeline-event-harness .fc-timeline-event')
 
-    if celtit[8] == "Available":
-        # with the given room number as a key, initialize a new dict
-        if roomNumber not in rooms:
-            rooms[roomNumber] = {}
-        # with the given date, initialize a new list
-        elif day not in rooms[roomNumber]:
-            rooms[roomNumber][day] = []
-        # add the time to the given day list and room number dict
-        else:
-            rooms[roomNumber][day].append(celtit[0])
+        for cell in cells:
+            # split the cell title by whitespace
+            # cell titles are in the format "hh:mmxm day, Month dd, yyyy - XXXX - XXXX"
+            celtit = cell.get_attribute('title').split()
+            room_number = int(celtit[6])
+            day = int(celtit[3].strip(','))
 
-print(rooms)
+            if celtit[8] == "Available":
+                # with the given room number as a key, initialize a new dict
+                if room_number not in rooms:
+                    rooms[room_number] = {}
+                # with the given date, initialize a new list
+                elif day not in rooms[room_number]:
+                    rooms[room_number][day] = []
+                # add the time to the given day list and room number dict
+                else:
+                    rooms[room_number][day].append(celtit[0])
 
-
-
-while True:
-    pass
+        print(rooms)
